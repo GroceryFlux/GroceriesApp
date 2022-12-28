334e0cbff0fbf6bd4128af41834f301cc6b1eaba
Sami Bassoumi <samiletuns@live.fr>
2022-12-28 17:37:03 +0100

    Initiated creation of list with state

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,68 @@
+/* eslint-disable indent */
 import React, { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import './App.css';
+import './input.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [isModalVisible, setIsModalVisible] = useState(true);
+  const [items, setItems] = useState(['']);
+  const [shoppingList, saveList] = useState([]);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <>
+    { isModalVisible ? 
+      <>
+      <form onSubmit={(event) => {
+            event.preventDefault();
+            const newArray = [];
+            for(let i = 2; i < event.target.length-1; i++){
+              newArray.push(event.target[i].value);
+              saveList(newArray);
+            }
+          } }>
+        <div className="flex justify-between pt-5 pl-5 pr-5">
+          <button onClick={() => setIsModalVisible(false)} className="text-5xl">&lt;-</button>
+          <button type="submit">Save</button>
+        </div>
+        <div className="flex justify-center">
+          <input placeholder="Title" value={shoppingList[0]} className="border text-center"></input>
+        </div>
+        <div className="flex flex-col justify-center border text-center">
+          <h1>List</h1>
+          <div>
+            { items.map((item, index) => {
+              if (index === items.length-1) {
+                return (
+                  <div 
+                  key={item} 
+                  onFocus={() => setItems([...items, 'new item'])} 
+                  className="text-gray-300">o <input placeholder="click to add"/>
+                  </div>
+                );
+              }
+
+            return (
+              <div key={item}>
+                o &nbsp;<input placeholder="Bananas" value={shoppingList[index+1]}></input>
+              </div>
+            );
+            }) }
+          </div>
+          {/* <div onClick={() => setItems([...items, 'new item'])} className="text-gray-300">o <input placeholder="click to add"/></div> */}
+        </div>
+      </form>
+
+      </> :
+      <>
+      <div className="flex w-full justify-end pt-5 pr-5">
+        <button className="text-5xl" onClick={() => setIsModalVisible(true)}>+</button>
+      </div> 
+      <div className="flex flex-col pl-5">
+        <h1>Existing lists</h1>
+        - {shoppingList[0]}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+      </>
+    }
+    </>
   );
 }
 
