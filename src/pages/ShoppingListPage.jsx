19ac7f00d191d818f816ab21abd8013843614299
Sami Bassoumi <samiletuns@live.fr>
2023-05-23 12:43:52 +0200

    Change ShoppingListPage as per simplification

--- src/pages/ShoppingListPage.jsx
+++ src/pages/ShoppingListPage.jsx
@@ -7,46 +7,52 @@ import DisplayBoughtItemsButton from '../components/ShoppingListPage/displayBoug
 
 function ShoppingListPage() {
   const shoppingList = useListsStore((state) => state.shoppingList);
+  
+
+  if (shoppingList.size === 0) {
+    return (
+      <>
+        <ShoppingListHeader />
+        <p className="w-3/4">Your shopping list is empty, please start adding items in your recurrent lists</p>
+      </>
+    );
+  }
+
   const showBoughtItems = useBoughtItemsStore((state) => state.showBoughtItems);
+  const boughtItems = [];
+  const unboughtItems = [];
+
+  shoppingList.forEach((item, itemId) => {
+    if (item.isBought) {
+      boughtItems.push(
+        <ItemLine
+          key={itemId}
+          itemID={itemId}
+          item={item}
+        />,
+      );
+      return;
+    }
+
+    unboughtItems.push(
+      <ItemLine
+        key={itemId}
+        itemID={itemId}
+        item={item}
+      />,
+    );
+  });
 
   return (
     <>
       <ShoppingListHeader />
-      {(shoppingList.size === 0) | (shoppingList.length === 0) ? (
-        <p className="w-3/4">Your shopping list is empty, please start adding items in your reccurent lists</p>
-      ) : (
-        <>
-          <h2>Basket</h2>
-          <ul className="mb-3 mt-3">
-            {[...shoppingList.entries()].map(([itemID, item]) =>
-              item.isBought === false ? (
-                <ItemLine
-                  key={itemID}
-                  itemID={itemID}
-                  item={item}
-                />
-              ) : null,
-            )}
-          </ul>
-          <div className="flex flex-row gap-3">
-            <h2>Completed</h2>
-            <DisplayBoughtItemsButton />
-          </div>
-          {showBoughtItems === true ? (
-            <ul className="mb-3 mt-3">
-              {[...shoppingList.entries()].map(([itemID, item]) =>
-                item.isBought === true ? (
-                  <ItemLine
-                    key={itemID}
-                    itemID={itemID}
-                    item={item}
-                  />
-                ) : null,
-              )}
-            </ul>
-          ) : null}
-        </>
-      )}
+      <h2>Basket</h2>
+      <ul className="mb-3 mt-3">{unboughtItems}</ul>
+      <div className="flex flex-row gap-3">
+        <h2>Completed</h2>
+        <DisplayBoughtItemsButton />
+      </div>
+      {showBoughtItems && <ul className="mb-3 mt-3">{boughtItems}</ul>}
     </>
   );
 }
