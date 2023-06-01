6869a428794180ce9e66c4e3ec3fba2c88330760
Sami Bassoumi <samiletuns@live.fr>
2023-06-01 16:06:44 +0200

    Changed NewItemForm in a single component (deleted AddItemButton and ItemTitleInput) and removed div

--- src/components/ItemsPage/NewItemForm.jsx
+++ src/components/ItemsPage/NewItemForm.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import ItemTitleInput from './ItemTitleInput';
 import { useListsStore } from '../../store/lists/lists';
-import AddItemButton from './AddItemButton';
+import { useThemeStore } from '../../store/theme/theme';
 
 function NewItemForm() {
   const listID = useListsStore((state) => state.selectedListID);
   const list = useListsStore((state) => state.existingLists).get(listID);
   const saveExistingLists = useListsStore((state) => state.saveExistingLists);
+  const theme = useThemeStore((state) => state.theme)
 
   return (
     <form
@@ -24,12 +24,19 @@ function NewItemForm() {
       }}
     >
       <div className="flex flex-row justify-center text-center my-4">
-        <div>
-          <ItemTitleInput />
-        </div>
-        <div>
-          <AddItemButton />
-        </div>
+        <input
+          className={`text-center border-solid border-y-2 rounded-l-lg border-l-2 border-blue-400 p-1 ${
+            theme === 'dark' ? 'bg-slate-700 text-slate-200' : ''
+          }`}
+          placeholder="Bananas"
+          defaultValue=""
+        />
+        <button
+          type="submit"
+          className="border-solid border-y-2 border-r-2 rounded-r-lg border-blue-400 text-blue-400 p-1"
+        >
+          &nbsp;<i className="fa-solid fa-circle-plus"></i>
+        </button>
       </div>
     </form>
   );
