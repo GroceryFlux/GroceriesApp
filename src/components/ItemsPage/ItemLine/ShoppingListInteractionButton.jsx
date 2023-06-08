f8b2a0fc4b900035cf85a4794311670c5057f3de
Sami Bassoumi <samiletuns@live.fr>
2023-06-08 10:02:45 +0200

    Ran lint and prettier

--- src/components/ItemsPage/ItemLine/ShoppingListInteractionButton.jsx
+++ src/components/ItemsPage/ItemLine/ShoppingListInteractionButton.jsx
@@ -3,20 +3,19 @@ import PropTypes from 'prop-types';
 import { useListsStore } from '../../../store/lists/lists';
 
 function getIcon(item) {
-  if(item.isBought) {
-    return <i className="fa-solid fa-check-double"></i>
+  if (item.isBought) {
+    return <i className="fa-solid fa-check-double"></i>;
   }
 
-  if(item.isOnShoppingList) {
-    return <i className="fa-solid fa-check"></i>
+  if (item.isOnShoppingList) {
+    return <i className="fa-solid fa-check"></i>;
   }
 
-  return <i className="fa-solid fa-plus"></i>
+  return <i className="fa-solid fa-plus"></i>;
 }
 
 function ShoppingListInteractionButton({ itemID, item }) {
   const toggleShoppingListItem = useListsStore((state) => state.toggleShoppingListItem);
-
 
   return (
     <div
