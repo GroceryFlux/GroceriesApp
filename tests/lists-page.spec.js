d50b80f71e90ae5703c5ca3e8317e660246e06de
Sami Bassoumi <samiletuns@live.fr>
2024-07-11 18:30:30 +0200

    Added a test to change the name of a list

--- tests/lists-page.spec.js
+++ tests/lists-page.spec.js
@@ -6,3 +6,19 @@ test('should have one recurrent list with items', async ({ pageWithRecurrentList
 
   await expect(recurrentList).toBeVisible();
 });
+
+test('should change the name of the list Breakie to Breakfast', async ({ pageWithRecurrentList }) => {
+
+  // Opens the recurent list
+  await pageWithRecurrentList.getByRole('button', { name: 'Breaky Strawberries, Oats, Joghurt' }).click()
+
+  // Clicks on the title
+  await pageWithRecurrentList.getByPlaceholder('Title').click();
+
+  // Changes title to Breakfast
+  await pageWithRecurrentList.getByPlaceholder('Title').fill('Breakfast');
+  await pageWithRecurrentList.getByPlaceholder('Title').press('Enter');
+
+  // Returns to recurrent lists to save
+  await pageWithRecurrentList.locator('.text-1xl').click();
+})
