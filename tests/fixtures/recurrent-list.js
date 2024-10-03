57a7d5e9efa1e6cb471996c06c6df98b0e8edaa7
Sami Bassoumi <samiletuns@live.fr>
2024-10-03 11:36:13 +0200

    Fixed some issues and robustness in some tests

--- tests/fixtures/recurrent-list.js
+++ tests/fixtures/recurrent-list.js
@@ -68,4 +68,106 @@ export const test = base.test.extend({
     // Use the fixture in the test
     await use(page);
   },
+
+  pageWithSimilarRecurrentListsName: async ({ page }, use) => {
+    // Go to the main page
+    await page.goto('http://localhost:5173/GroceriesApp/ ');
+
+    // Click the button to create the first recurrent list
+    await page.getByRole('button').nth(2).click();
+
+    // Write the title
+    await page.getByPlaceholder('Title').click();
+    await page.getByPlaceholder('Title').fill('Breaky');
+
+    // Add items
+    await page.getByPlaceholder('Strawberries').fill('500 g Strawberries');
+    await page.getByPlaceholder('Strawberries').press('Enter');
+
+    // Go back to the main page
+    await page.locator('.text-1xl').click();
+
+    // Click the button to create the first recurrent list
+    await page.getByRole('button').nth(2).click();
+
+    // Write the title
+    await page.getByPlaceholder('Title').click();
+    await page.getByPlaceholder('Title').fill('BreakyWeekEnd');
+
+    // Add items
+    await page.getByPlaceholder('Strawberries').fill('5 Croissant');
+    await page.getByPlaceholder('Strawberries').press('Enter');
+
+    // Go back to the main page
+    await page.locator('.text-1xl').click();
+
+    // Click the button to create the second recurrent list
+    await page.getByRole('button').nth(2).click();
+
+    // Write the title
+    await page.getByPlaceholder('Title').click();
+    await page.getByPlaceholder('Title').fill('Lunch');
+
+    // Add items
+    await page.getByPlaceholder('Strawberries').fill('300 g Pasta');
+    await page.getByPlaceholder('Strawberries').press('Enter');
+
+    // Go back to the main page
+    await page.locator('.text-1xl').click();
+
+    // Use the fixture in the test
+    await use(page);
+  },
+
+  pageWithSameItemInDifferentLists: async ({ page }, use) => {
+    // Go to the main page
+    await page.goto('http://localhost:5173/GroceriesApp/ ');
+
+    // Click the button to create the first recurrent list
+    await page.getByRole('button').nth(2).click();
+
+    // Write the title
+    await page.getByPlaceholder('Title').click();
+    await page.getByPlaceholder('Title').fill('Breaky');
+
+    // Add items
+    await page.getByPlaceholder('Strawberries').fill('500 g Strawberries');
+    await page.getByPlaceholder('Strawberries').press('Enter');
+
+    // Go back to the main page
+    await page.locator('.text-1xl').click();
+
+    // Click the button to create the first recurrent list
+    await page.getByRole('button').nth(2).click();
+
+    // Write the title
+    await page.getByPlaceholder('Title').click();
+    await page.getByPlaceholder('Title').fill('Fruits');
+
+    // Add items
+    await page.getByPlaceholder('Strawberries').fill('200 g Strawberries');
+    await page.getByPlaceholder('Strawberries').press('Enter');
+
+    // Go back to the main page
+    await page.locator('.text-1xl').click();
+
+    // Click the button to create the second recurrent list
+    await page.getByRole('button').nth(2).click();
+
+    // Write the title
+    await page.getByPlaceholder('Title').click();
+    await page.getByPlaceholder('Title').fill('Lunch');
+
+    // Add items
+    await page.getByPlaceholder('Strawberries').fill('300 g Pasta');
+    await page.getByPlaceholder('Strawberries').press('Enter');
+
+    // Go back to the main page
+    await page.locator('.text-1xl').click();
+
+    // Use the fixture in the test
+    await use(page);
+  },
+
+  
 });
--- tests/lists-page.spec.js
+++ tests/lists-page.spec.js
@@ -20,6 +20,9 @@ test('should change the name of the list Breakie to Breakfast', async ({ pageWit
 
   // Returns to recurrent lists to save
   await pageWithRecurrentList.locator('.text-1xl').click();
+
+  // Checks the list name has changed to Breakfast
+  await expect(pageWithRecurrentList.getByRole('button', { name: 'Breakfast' })).toBeVisible();
 });
 
 test('should delete an existing list then cancel the delete', async ({ pageWithRecurrentList }) => {
@@ -47,13 +50,16 @@ test('should delete an existing list then confirm the delete', async ({ pageWith
 });
 
 test('should check the change in colors when setting dark theme', async ({ pageWithRecurrentList }) => {
-  //locates the background when in light theme
-  const backgroundLight = pageWithRecurrentList.getByText('Groceries0Recurrent');
+  //locates the background when in initial (light) theme
+  const html = pageWithRecurrentList.locator('html');
 
   //gets the color from the background in light theme
-  const backgroundColorLight = await backgroundLight.evaluate((e) => {
-    return window.getComputedStyle(e).getPropertyValue('color');
+  const initialBackgroundColor = await html.evaluate((e) => {
+    return window.getComputedStyle(e).getPropertyValue('background-color');
   });
+
+  //checks the color is light
+  expect(initialBackgroundColor).toEqual('rgb(241, 244, 242)');
 
   //changes theme to dark
   await pageWithRecurrentList
@@ -63,16 +69,13 @@ test('should check the change in colors when setting dark theme', async ({ pageW
     .first()
     .click();
 
-  //locates the background when in dark theme
-  const backgroundDark = pageWithRecurrentList.getByText('Groceries0Recurrent');
-
   //gets the color from the background in dark theme
-  const backgroundColorDark = await backgroundDark.evaluate((e) => {
-    return window.getComputedStyle(e).getPropertyValue('color');
+  const backgroundColorDark = await html.evaluate((e) => {
+    return window.getComputedStyle(e).getPropertyValue('background-color');
   });
 
-  //checks the color has changed
-  await expect(backgroundColorLight).not.toEqual(backgroundColorDark);
+  //checks the color is dark
+  await expect(backgroundColorDark).toEqual('rgb(15, 23, 41)');
 
   //changes theme to light
   await pageWithRecurrentList
@@ -82,16 +85,13 @@ test('should check the change in colors when setting dark theme', async ({ pageW
     .first()
     .click();
 
-  //locates the background when back in light theme
-  const backgroundLight2 = pageWithRecurrentList.getByText('Groceries0Recurrent');
-
-  //gets the color from the background when back in light theme
-  const backgroundColorLight2 = await backgroundLight2.evaluate((e) => {
-    return window.getComputedStyle(e).getPropertyValue('color');
+  //gets the color from the background light theme
+  const backgroundColorLight = await html.evaluate((e) => {
+    return window.getComputedStyle(e).getPropertyValue('background-color');
   });
 
   //checks the theme is back to the same light color as before
-  await expect(backgroundColorLight).toEqual(backgroundColorLight2);
+  await expect(backgroundColorLight).toEqual('rgb(241, 244, 242)');
 });
 
 test('should sort the list by lastest edited', async ({ pageWithTwoRecurrentLists }) => {
@@ -104,6 +104,9 @@ test('should sort the list by lastest edited', async ({ pageWithTwoRecurrentList
     .getByRole('button')
     .nth(1)
     .click();
+
+  //Checks if the first item on the list page is the first alphabetically
+  await expect(pageWithTwoRecurrentLists.getByRole('listitem').nth(0)).toHaveText(['BreakyStrawberries']);
 
   //Clicks on button to change to last edited sorting
   await pageWithTwoRecurrentLists.locator('.relative > button').click();
@@ -120,6 +123,20 @@ test('should sort the list by lastest edited', async ({ pageWithTwoRecurrentList
 });
 
 test('should sort the list by alphabetic order', async ({ pageWithTwoRecurrentLists }) => {
+
+  //Clicks on button to change to last edited sorting
+  await pageWithTwoRecurrentLists.locator('.relative > button').click();
+  await pageWithTwoRecurrentLists.getByRole('button', { name: 'Date' }).click();
+  await pageWithTwoRecurrentLists
+    .locator('div')
+    .filter({ hasText: /^Recurrent listsDateA-Z$/ })
+    .getByRole('button')
+    .nth(1)
+    .click();
+
+  //Checks if first item on the list page is the last created/edited one
+  await expect(pageWithTwoRecurrentLists.getByRole('listitem').nth(0)).toHaveText(['LunchPasta']);
+
   //Clicks on button to change to alphabetic order
   await pageWithTwoRecurrentLists.locator('.relative > button').click();
   await pageWithTwoRecurrentLists.getByRole('button', { name: 'A-Z' }).click();
@@ -134,16 +151,16 @@ test('should sort the list by alphabetic order', async ({ pageWithTwoRecurrentLi
   await expect(pageWithTwoRecurrentLists.getByRole('listitem').nth(0)).toHaveText(['BreakyStrawberries']);
 });
 
-test('should search for a list called Breaky', async ({ pageWithTwoRecurrentLists }) => {
-  await pageWithTwoRecurrentLists.locator('button:nth-child(3)').first().click();
-  await pageWithTwoRecurrentLists.getByPlaceholder('Looking for a list or an item').click();
-  await pageWithTwoRecurrentLists.getByPlaceholder('Looking for a list or an item').fill('Breaky');
-  await expect(pageWithTwoRecurrentLists.getByRole('listitem')).toHaveText(['BreakyStrawberries']);
+test('should search for a list called Breaky', async ({ pageWithSimilarRecurrentListsName }) => {
+  await pageWithSimilarRecurrentListsName.locator('button:nth-child(3)').first().click();
+  await pageWithSimilarRecurrentListsName.getByPlaceholder('Looking for a list or an item').click();
+  await pageWithSimilarRecurrentListsName.getByPlaceholder('Looking for a list or an item').fill('Breaky');
+  await expect(pageWithSimilarRecurrentListsName.getByRole('listitem')).toHaveText(['BreakyWeekEndCroissant', 'BreakyStrawberries']);
 });
 
-test('should search for an item called Strawberries', async ({ pageWithTwoRecurrentLists }) => {
-  await pageWithTwoRecurrentLists.locator('button:nth-child(3)').first().click();
-  await pageWithTwoRecurrentLists.getByPlaceholder('Looking for a list or an item').click();
-  await pageWithTwoRecurrentLists.getByPlaceholder('Looking for a list or an item').fill('Strawberries');
-  await expect(pageWithTwoRecurrentLists.getByRole('listitem')).toHaveText(['BreakyStrawberries']);
+test('should search for an item called Strawberries', async ({ pageWithSameItemInDifferentLists }) => {
+  await pageWithSameItemInDifferentLists.locator('button:nth-child(3)').first().click();
+  await pageWithSameItemInDifferentLists.getByPlaceholder('Looking for a list or an item').click();
+  await pageWithSameItemInDifferentLists.getByPlaceholder('Looking for a list or an item').fill('Strawberries');
+  await expect(pageWithSameItemInDifferentLists.getByRole('listitem')).toHaveText(['FruitsStrawberries', 'BreakyStrawberries']);
 });
